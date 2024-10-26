a6beb5f24df22b57fe6be58640d79abbe5e29e9a
Ultime <ultimeh@outlook.com>
2024-10-26 17:45:02 -0400

    stuff

--- baseBot/Program.cs
+++ baseBot/Program.cs
@@ -2,8 +2,6 @@
 {
     class Program
     {
-        //public static AppData appData = new AppData(); //class static pour global variable si besoin
-
         static async Task Main(string[] args) //string[] args peu etre utile pour argument au demarage:   bot.exe arg1 arg2 arg3
 		{
 			Bot bot = new Bot();
